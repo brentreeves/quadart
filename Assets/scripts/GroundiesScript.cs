1bd8afc6ab956ee0e0f16311d289b57e99486d35
brentreeves <brent@twinbear.com>
2016-05-01 15:30:27 -0500

    First checkin

---
+++ Assets/scripts/GroundiesScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundiesScript : MonoBehaviour {
+    public float Rodcount;
+    public float BEGINX = -9.0f;
+    public float BEGINY = 0f;
+    public float BEGINZ = -9.0f;
+    public int CLUSTERSIZE = 3;
+    public float CLUSTERSPACE = .6f;
+    
+    public ArrayList rods = new ArrayList ();
+
+    private GameObject pattern;
+    private GameObject aRod;
+    private Color cc;
+
+    void makeRow( float beginX, float beginY, float beginZ) {
+					      
+	//	pattern = Resources.Load ("Rod") as GameObject;
+	pattern = Resources.Load ("RodSquare") as GameObject;
+	for (int blocks=0; blocks< 32; blocks++) {
+	    for (int i=0; i< CLUSTERSIZE; i++) {
+		for (int j=0; j< CLUSTERSIZE; j++) {
+
+		    // or... create one from scratch instead
+		    //		    GameObject aRod = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		    //		    cylinder.transform.position = new Vector3(-2, 1, 0);
+		    
+		    Vector3 v;
+		    float xx = (float) (i * CLUSTERSPACE);
+		    float yy = (float) (j * CLUSTERSPACE);
+
+		    cc.r = .1f;
+		    cc.g = .1f;
+		    cc.b = .1f;
+		    for (int k=0; k<10; k++) {
+			aRod = Instantiate (pattern);
+			v = new Vector3 ( (float) (beginX + (1.0 * blocks * (CLUSTERSIZE * CLUSTERSPACE)) + xx), 
+					  (float) (.5 + k), 
+					  (float) (beginZ + (1.7 * blocks * (CLUSTERSIZE * CLUSTERSPACE)) + yy));
+			aRod.transform.position = v;
+			aRod.GetComponent<Renderer>().material.color = cc; // Color.grey;
+
+			rods.Add ( (GameObject) aRod);
+		    }
+
+		    //			    }
+		}
+	    }
+	}
+    }
+
+    // Use this for initialization
+    void Start () {
+	Debug.Log ("GroundScript Start()");
+
+	makeRow(-30, 0, -100);
+	makeRow(-50, 0, -76);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	//	Debug.Log ("GroundiesScript Update() rods: " + rods.Count);
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+    private int rodCount;
+    private GameObject ground;
+    private GroundiesScript groundiesScript;
+    private ArrayList rods;
+    private float rr;
+    private GameObject aRod;
+
+    // Use this for initialization
+    void Start () {
+	rb = GetComponent<Rigidbody> ();
+	ground = GameObject.Find("Ground");
+	groundiesScript = ground.GetComponent<GroundiesScript>();
+	rods = groundiesScript.rods;
+    }
+	
+    // Update is called once per frame
+    void Update () {
+
+	//	    mess with some lights
+    }
+
+    void FixedUpdate () {
+	if (Input.GetKeyDown("z")) {
+	    int rodCount = groundiesScript.rods.Count;
+	    Debug.Log("z key pressed: " + rodCount);
+
+	    for (int i=0; i< 10; i++) {
+		float rr = Random.Range(0F, 1.0F);
+		int ir = (int) Mathf.Floor( rr * rodCount );
+		//		Debug.Log("z updating item " + ir);
+		//		aRod = (GameObject) groundiesScript.rods[ ir ];
+		//		aRod.GetComponent<Renderer>().material.color = Color.white;
+		aRod = (GameObject) rods[ ir ];
+		aRod.GetComponent<Renderer>().material.color = Color.white;
+	    }
+	    
+	}
+	if (Input.GetKeyDown("x")) {
+	    int rodCount = groundiesScript.rods.Count;
+
+	    Debug.Log("x key pressed: " + rodCount);
+	    
+	    for (int i=0; i < rodCount; i++) {
+		aRod = (GameObject) rods [ i ];
+		aRod.GetComponent<Renderer>().material.color = Color.black;
+	    }
+	}
+
+	float moveHorizontal = Input.GetAxis ("Horizontal");
+	float moveVertical = Input.GetAxis ("Vertical");
+	Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+	rb.AddForce (movement * speed);
+    }
+
+}
